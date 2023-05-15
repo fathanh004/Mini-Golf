f9c2ebc3b9879bc1f5b250cac539c58c45fa4bef
fathanh004 <fathanhidayatullah.2020@student.uny.ac.id>
2023-05-15 13:49:27 +0700

    add main menu and level selector, finishing

---
+++ Assets/MainMenuCanvas.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuCanvas : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenuPanel;
+    [SerializeField] GameObject levelSelectorPanel;
+    [SerializeField] GameObject optionPanel;
+
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void Play()
+    {
+        mainMenuPanel.SetActive(false);
+        levelSelectorPanel.SetActive(true);
+    }
+
+    public void Option()
+    {
+        mainMenuPanel.SetActive(false);
+        optionPanel.SetActive(true);
+    }
+
+    public void Back()
+    {
+        mainMenuPanel.SetActive(true);
+        levelSelectorPanel.SetActive(false);
+        optionPanel.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
